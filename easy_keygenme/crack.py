e49882568c024dd9f953fe82e7dee464e4c5edaa
Neel <neel.patel6573@gmail.com>
2020-07-07 18:29:35 -0400

    cracks easy_keygenme

--- easy_keygenme/crack.py
+++ easy_keygenme/crack.py
@@ -5,4 +5,4 @@ for ch in username:
 	encch = (ord(ch)-97+K) < 25 and chr(((ord(ch)-97+K)%26)+97) or chr(((ord(ch)-97+K-26)%26)+97)
 	key.append(encch)
 
-print("".join(key))
+print("The Key is: {}".format("".join(key)))
