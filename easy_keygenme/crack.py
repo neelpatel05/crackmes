26edb7bd7e1858542aab3d712555e930aaa81422
Neel <neel.patel6573@gmail.com>
2020-07-07 18:25:01 -0400

    cracks keygenme

---
+++ easy_keygenme/crack.py
@@ -0,0 +1,8 @@
+username = raw_input("Enter the username lowercase between 3 to 9: ")
+key = []
+K = len(username)
+for ch in username:
+	encch = (ord(ch)-97+K) < 25 and chr(((ord(ch)-97+K)%26)+97) or chr(((ord(ch)-97+K-26)%26)+97)
+	key.append(encch)
+
+print("".join(key))
